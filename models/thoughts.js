fb9f058de9d88d7279a2adb6e9e5ca0a6182cbd7
nrasch86 <145396887+nrasch86@users.noreply.github.com>
2024-03-25 15:50:24 -0400

    Updates thoughts.js structure

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,34 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-//Schema for new thoughts
+// Schema for reactions to thoughts
+const reactionSchema = new Schema({
+  reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+  reactionBody: { type: String, required: true, maxlength: 280 },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a') },
+}, {
+  toJSON: { getters: true },
+  id: false,
+});
+
+// Schema for new thoughts
 const thoughtSchema = new Schema({
   thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
   createdAt: { type: Date, default: Date.now, get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a') },
   username: { type: String, required: true },
-  reactions: [reactionSchema],
+  reactions: [reactionSchema], 
 }, {
   toJSON: { virtuals: true, getters: true },
   id: false,
 });
 
-// Schema for reactions to thoughts
-const reactionSchema = new Schema({
-    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
-    reactionBody: { type: String, required: true, maxlength: 280 },
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a') },
-  }, {
-    toJSON: { getters: true },
-    id: false,
-});
-
-//Function for reaction count
+// Function for reaction count
 thoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-//Creating model for Mongoose
+// Creating model for Mongoose
 const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
